eb3c42b0cb5eee67a883273b0f4305a5ebc60728
pulkitarora21 <pulkitarora21@gmail.com>
2014-07-03 18:23:17 +0530

    queries working on half an hour basis

--- demoapp2/views.py
+++ demoapp2/views.py
@@ -4,8 +4,10 @@ import json,csv
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from newapp.models import *
-
+from time import strftime
+from datetime import *
 htmlsrc=""
+time="2015-07-03-17:20:10"
 def visible(element):
     if element.parent.name in ['style', 'script', '[document]', 'head', 'title']:
         return False
@@ -20,17 +22,18 @@ def index(request):
 	#counterget.count = countget
 	#counterget.save()
 	
-def sslwrap(func):
-    @wraps(func)
-    def bar(*args, **kw):
-        kw['ssl_version'] = ssl.PROTOCOL_TLSv1
-        return func(*args, **kw)
-    return bar
-
-ssl.wrap_socket = sslwrap(ssl.wrap_socket)
-	global htmlsrc
+	global htmlsrc,time
+	d=datetime.now()-timedelta(minutes=30)
+	fromtime=d.strftime("%Y-%m-%d-%H:%M:%S")
 	# response = urllib2.urlopen('http://127.0.0.1:8888/abc')
-	response = urllib2.urlopen('https://192.168.1.40:9119/client?uid=client_id&last=10&token=98a4bafbb35b3527c524ac5d73e9ff8e5e56c6caed488b259682feb7d0a1d192')
+	url = "https://192.168.1.40:9119/client?uid=client_id&from="
+	url = url+fromtime+"&to="
+	time=strftime("%Y-%m-%d-%H:%M:%S")
+	url=url+time+"&format=yyyy-mm-dd-hh24:mi:ss&token=98a4bafbb35b3527c524ac5d73e9ff8e5e56c6caed488b259682feb7d0a1d192"
+	# &from=2014-07-01-23:10:10&to=2014-07-02-23:10:15&format=yyyy-mm-dd-hh24:mi:ss
+	# response = urllib2.urlopen('https://192.168.1.40:9119/client?uid=client_id&last=10&token=98a4bafbb35b3527c524ac5d73e9ff8e5e56c6caed488b259682feb7d0a1d192')
+	print url
+	response = urllib2.urlopen(url)
 	htmlsrc = response.read()
 	
 	# http = urllib3.PoolManager()
